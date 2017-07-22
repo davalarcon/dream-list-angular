8ca4079d982f17c2365c770f22080ad0702a8a08
davalarcon <zilentstorm@gmail.com>
2017-07-22 19:07:25 -0400

    working in signup

--- src/app/login-signup/login-signup.component.ts
+++ src/app/login-signup/login-signup.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from '../services/auth.service';
+
 @Component({
   selector: 'app-login-signup',
   templateUrl: './login-signup.component.html',
   styleUrls: ['./login-signup.component.css']
 })
 export class LoginSignupComponent implements OnInit {
+
+  nameValue: string;
+  lastNameValue: string;
+  birthdayValue: string;
+  emailValue: string;
+  passwordValue: string;
 
   constructor() { }
 
